28e454267f97472c9221960f2b1079aa780d96e3
Your Name <you@example.com>
2025-08-13 18:22:19 -0500

    Changes

--- CTkFileDialog/__init__.py
+++ CTkFileDialog/__init__.py
@@ -1,14 +1,14 @@
 '''
 File dialog created in custom Tkinter and inspired by the Tkinter dialog.
 
-- Version: 0.3.0
+- Version: 0.3.1
 - Author: Flick
 - Github: https://github.com/FlickGMD/CTkFileDialog
 '''
 from ._functions import askopenfilename, askdirectory, askopenfile, askopenfiles, askopenfilenames, asksaveasfile, asksaveasfilename 
 from . import Constants
 
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __all__ = [
         'askopenfilename',
         'askdirectory',
